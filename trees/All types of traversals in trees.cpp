2308fd0ed60e561a8414316f8b019b2295360f70
AkshayKohad <65406900+AkshayKohad@users.noreply.github.com>
2021-05-10 12:21:21 +0530

    Update All types of traversals in trees.cpp

--- trees/All types of traversals in trees.cpp
+++ trees/All types of traversals in trees.cpp
@@ -49,3 +49,26 @@ PostorderTraversal(root->left);
 }
 } 
 
+
+// Level Order Traversal
+void LevelOrder(Node*root){
+if(root==NULL)
+  return;
+  
+  queue<Node*>q;
+  q.push(root);
+  
+  while(q.empty()==false){
+  Node*curr = q.front();
+    q.pop();
+    cout<<curr->key<<" ";
+    
+    if(curr->left!=NULL){
+    q.push(curr->left);
+    }
+    
+    if(curr->right!=NULL){
+    q.push(curr->right);
+    }
+  }
+}

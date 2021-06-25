ece9ee2b4fbd903e377e520d42bfd66bddcfb833
AkshayKohad <65406900+AkshayKohad@users.noreply.github.com>
2021-06-25 16:06:47 +0530

    Create Fibonacci Numbers - Top Down DP(Memoization).cpp

---
+++ Dynamic Programming/Fibonacci Numbers - Top Down DP(Memoization).cpp
@@ -0,0 +1,33 @@
+class Solution
+{
+    public:
+    
+    long long solution(int n,vector<long long>&res)
+    {
+        if(res[n]==-1)
+        {
+            long long val;
+            
+            if(n==0 || n==1)
+            return n;
+            
+            else
+            val = solution(n-1,res)+solution(n-2,res);
+            
+            res[n]=val;
+        }
+        
+        return res[n];
+    }
+    //Function to find the nth fibonacci number using top-down approach.
+    long long findNthFibonacci(int number)
+    {
+        // Your Code Here
+        vector<long long>res(number+1,-1);
+        solution(number,res);
+        if(number==1 || number==0)
+        return number;
+        return res[number];
+    }
+
+};

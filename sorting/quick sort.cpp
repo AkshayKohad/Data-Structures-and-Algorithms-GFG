755e1c2ce9f65412c20c468921d1c4f9faac852f
AkshayKohad <65406900+AkshayKohad@users.noreply.github.com>
2021-07-01 22:48:31 +0530

    Create quick sort.cpp

---
+++ sorting/quick sort.cpp
@@ -0,0 +1,80 @@
+#include <stdio.h>
+#include <bits/stdc++.h>
+using namespace std;
+int partition (int arr[], int low, int high);
+/* The main function that implements QuickSort
+ arr[] --> Array to be sorted, low  --> Starting index, high  --> Ending index */
+void quickSort(int arr[], int low, int high)
+{
+    if (low < high)
+    {
+        /* pi is partitioning index, arr[p] is now at right place */
+        int pi = partition(arr, low, high);
+        // Separately sort elements before partition and after partition
+        quickSort(arr, low, pi - 1);
+        quickSort(arr, pi + 1, high);
+    }
+}
+/* Function to print an array */
+void printArray(int arr[], int size)
+{
+    int i;
+    for (i=0; i < size; i++)
+        printf("%d ", arr[i]);
+    printf("\n");
+}
+// Driver program to test above functions
+int main()
+{
+    int arr[1000],n,T,i;
+    scanf("%d",&T);
+    while(T--){
+    scanf("%d",&n);
+    for(i=0;i<n;i++)
+      scanf("%d",&arr[i]);
+    quickSort(arr, 0, n-1);
+    printArray(arr, n);
+    }
+    return 0;
+}// } Driver Code Ends
+
+
+/* The main function that implements QuickSort
+ arr[] --> Array to be sorted,    low  --> Starting index,   high  --> Ending index
+void quickSort(int arr[], int low, int high) {
+    if (low < high)     {
+        // pi is partitioning index, arr[p] is now  at right place
+        int pi = partition(arr, low, high);
+        // Separately sort elements before / partition and after partition
+        quickSort(arr, low, pi - 1);
+        quickSort(arr, pi + 1, high);
+    }
+}*/
+/* This function takes last element as pivot, places  the pivot element 
+   at its correct position in sorted  array, and places all smaller (smaller
+   than pivot) to left of pivot and all greater elements to right  of pivot */
+int partition (int a[], int l, int h)
+{
+  //  int p=rand(l,h);
+    
+    //swap(a[p],a[h]);
+    int p=h;
+    int i=l-1;
+    int j=l;
+    
+    while(j<=h)
+    {
+        if(a[j]<a[p])
+        {
+            i++;
+            swap(a[i],a[j]);
+            
+        }
+        
+        j++;
+    }
+    
+    swap(a[i+1],a[h]);
+    return i+1;
+   // Your code here
+}

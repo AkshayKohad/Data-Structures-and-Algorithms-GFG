0ab3bea47c44cca2450b81c70283c953e0971edc
AkshayKohad <65406900+AkshayKohad@users.noreply.github.com>
2021-07-25 12:45:12 +0530

    Create 0 - 1 Knapsack Problem(Memoization).cpp

---
+++ Dynamic Programming/0 - 1 Knapsack Problem(Memoization).cpp
@@ -0,0 +1,32 @@
+int knapSackRec(int W, int wt[],
+                int val[], int i,
+                int** dp)
+{
+    // base condition
+    if (i < 0)
+        return 0;
+    if (dp[i][W] != -1)
+        return dp[i][W];
+ 
+    if (wt[i] > W) {
+ 
+        // Store the value of function call
+        // stack in table before return
+        dp[i][W] = knapSackRec(W, wt,
+                               val, i - 1,
+                               dp);
+        return dp[i][W];
+    }
+    else {
+        // Store value in a table before return
+        dp[i][W] = max(val[i]
+                      + knapSackRec(W - wt[i],
+                                   wt, val,
+                                   i - 1, dp),
+                       knapSackRec(W, wt, val,
+                                   i - 1, dp));
+ 
+        // Return value of table after storing
+        return dp[i][W];
+    }
+}

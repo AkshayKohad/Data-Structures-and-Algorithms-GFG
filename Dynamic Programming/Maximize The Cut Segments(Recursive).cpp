3679a9fcc4bb4f0bbc788671b0a08d6f51584e86
AkshayKohad <65406900+AkshayKohad@users.noreply.github.com>
2021-08-23 18:53:26 +0530

    Create Maximize The Cut Segments(Recursive).cpp

---
+++ Dynamic Programming/Maximize The Cut Segments(Recursive).cpp
@@ -0,0 +1,73 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+
+
+ // } Driver Code Ends
+
+
+class Solution
+{
+    public:
+    int solution(int n,int x,int y,int z)
+    {
+     if(n<0)
+        return 0;
+        
+        if(n==0)
+        return 1;
+    int res1 = solution(n-x,x,y,z);
+     
+    int res2 = solution(n-y,x,y,z);
+    
+    int res3 = solution(n-z,x,y,z);
+    
+    
+      
+        int ans1 = max( res1,res2);
+        int ans = max(ans1,res3);
+      
+        if(ans==0)
+        return ans;
+        
+        return 1+ ans;   
+    }
+    //Function to find the maximum number of cuts.
+    int maximizeTheCuts(int n, int x, int y, int z)
+    {
+        //Your code here
+        int res = solution(n,x,y,z);
+       
+     // cout<<res<<endl;
+      
+      if(res==0)
+        return res;
+        
+        return res-1;
+        
+    }
+};
+
+// { Driver Code Starts.
+int main() {
+    
+    //taking testcases
+    int t;
+    cin >> t;
+    while(t--)
+    {
+        //taking length of line segment
+        int n;
+        cin >> n;
+        
+        //taking types of segments
+        int x,y,z;
+        cin>>x>>y>>z;
+        Solution obj;
+        //calling function maximizeTheCuts()
+        cout<<obj.maximizeTheCuts(n,x,y,z)<<endl;
+
+    }
+
+	return 0;
+}  // } Driver Code Ends
